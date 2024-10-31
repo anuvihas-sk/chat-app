5be28178aa91032bd96927439727fd1e5929ffa3
anuvihas <vihasoffc@gmail.com>
2024-10-31 20:52:08 +0530

    loading page

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Spinner from 'react-native-loading-spinner-overlay';
 
 export default function Home() {
     return (
-        <View>
-            <Text>Hello World</Text>
-        </View>
+        <View style={{ flex: 1 }}>
+        <Spinner
+          visible={true}
+          textContent={'Wait karo ji...'}
+          textStyle={{ color: 'white' }}
+          overlayColor="black"
+        />
+      </View>
     )
 }
